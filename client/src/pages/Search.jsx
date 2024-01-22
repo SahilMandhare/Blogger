ac4ba9f515cd637a4cb75407514eeb1c005294f7
unknown <sahilmandhare25@gmail.com>
2024-01-23 00:33:12 +0530

    Search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,93 +1,112 @@
-import React, { useEffect, useState } from 'react'
-import { useParams, useSearchParams } from 'react-router-dom'
-import ListingItem from '../components/ListingItem'
+import React, { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 const Search = () => {
+  const [query, setQuery] = useState({
+    search: '',
+    type: "all"
+  });
 
-    const [query, setQuery] = useState(null)
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const search = params.get("search");
 
-    useEffect(() => {
-        
-        const params = new URLSearchParams(location.search)
-        const search = params.get('search')
+    console.log(search)
 
-        setQuery({search})
-    }, [location.search])
+    setQuery({...query, search: params.get("search")});
+  }, [location.search]);
 
-    const handleSubmit = () => {
-        
-    }
+  const handleSubmit = (e) => {
 
-    console.log(query)
+    e.preventefault()
+
+    const params = new URLSearchParams(location.search);
+    params.set('search', query.search)
+
+    console.log("s"+query)
+  };
+  const handleChange = (e) => {
+
+    setQuery({...query, [e.target.id]: e.target.value})
+  };
+
+  const listings = [{
+    _id: "sndkjnsd",
+  },{
+    _id: "sndkjnsd",
+  }]
+
+  console.log(query);
 
   return (
-    <div className='flex flex-col md:flex-row'>
-      <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
-        <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
-          <div className='flex items-center gap-2'>
-            <label className='whitespace-nowrap font-semibold'>
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap font-semibold">
               Search Term:
             </label>
             <input
-              type='text'
-              id='searchTerm'
-              placeholder='Search...'
-              className='border rounded-lg p-3 w-full'
-              value={sidebardata.searchTerm}
+              type="text"
+              id="search"
+              placeholder="Search..."
+              className="border rounded-lg p-3 w-full"
+              defaultValue={query.search}
               onChange={handleChange}
             />
           </div>
-          <div className='flex items-center gap-2'>
-            <label className='font-semibold'>Sort:</label>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Sort:</label>
             <select
               onChange={handleChange}
-              defaultValue={'created_at_desc'}
-              id='sort_order'
-              className='border rounded-lg p-3'
+              defaultValue={"all"}
+              id="type"
+              className="border rounded-lg p-3"
             >
-              <option value='regularPrice_desc'>Price high to low</option>
-              <option value='regularPrice_asc'>Price low to hight</option>
-              <option value='createdAt_desc'>Latest</option>
-              <option value='createdAt_asc'>Oldest</option>
+              <option value="regularPrice_desc">Price high to low</option>
+              <option value="regularPrice_asc">Price low to hight</option>
+              <option value="createdAt_desc">Latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
-          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
             Search
           </button>
         </form>
       </div>
-      <div className='flex-1'>
-        <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
+      <div className="flex-1">
+        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
           Listing results:
         </h1>
-        <div className='p-7 flex flex-wrap gap-4'>
-          {!loading && listings.length === 0 && (
-            <p className='text-xl text-slate-700'>No listing found!</p>
+        <div className="p-7 flex flex-wrap gap-4">
+          {/* {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found!</p>
           )}
           {loading && (
-            <p className='text-xl text-slate-700 text-center w-full'>
+            <p className="text-xl text-slate-700 text-center w-full">
               Loading...
             </p>
-          )}
+          )} */}
 
-          {!loading &&
+          {/* {!loading &&
             listings &&
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
-            ))}
+            ))} */}
 
-          {showMore && (
+          {/* {showMore && (
             <button
               onClick={onShowMoreClick}
-              className='text-green-700 hover:underline p-7 text-center w-full'
+              className="text-green-700 hover:underline p-7 text-center w-full"
             >
               Show more
             </button>
-          )}
+          )} */}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
